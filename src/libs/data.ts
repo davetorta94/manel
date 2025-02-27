cf6badd712766c89f647d8db4bc3b4bc4044bb87
David <david.torta.olah@gmail.com>
2025-02-27 18:58:37 +0100

    update

--- src/libs/data.ts
+++ src/libs/data.ts
@@ -18,8 +18,8 @@ const categories = [
 ]
 
 const videos = [
-    { id: 1, nombre: "Ruta 99 (parte 1)", url: "https://www.youtube.com/watch?v=x3c9vZb5lA0&t=1s", thumb: "/thumb-1.png" },
-    { id: 2, nombre: "Corremos el Trial de Higueruela (Albacete) 2024", url: "https://www.youtube.com/watch?v=z2k0x2NGluM&t=8s", thumb: "/thumb-2.png" },
+    { id: 1, nombre: "Ruta 99 (parte 1)", url: "https://www.youtube.com/watch?v=x3c9vZb5lA0&t=1s", thumb: "/thumb-1.webp" },
+    { id: 2, nombre: "Corremos el Trial de Higueruela (Albacete) 2024", url: "https://www.youtube.com/watch?v=z2k0x2NGluM&t=8s", thumb: "/thumb-2.webp" },
     
 
   ];
