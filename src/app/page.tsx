ca95cd00bcb10ee460a389e50c53f782b9f6a0a9
David <david.torta.olah@gmail.com>
2025-02-27 18:39:17 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import {Cellphone} from "../components/cellphone";
-import { useState } from "react";
 import { categories } from "@/libs/data";
 import { Card } from "@/components/card";
 import { montserrat } from "@/fonts/fonts";
