f2a23bae0b7bec1754926da950bb51c26c5d50d0
David <david.torta.olah@gmail.com>
2025-03-27 17:48:52 +0100

    update

--- src/components/social-media.tsx
+++ src/components/social-media.tsx
@@ -1,21 +1,36 @@
 import Image from "next/image";
 import { NumberTicker } from "./magicui/number-ticker";
 
-export function SocialMedia() {
+export const SocialMedia = () => {
   return (
     <>
-    <div className="flex flex-row">
+    <div className="flex lg:flex-row flex-col align-middle">
+
+    {/*<div className="flex flex-row px-5">
 
     <Image src='/youtube-color-svgrepo-com.svg' width={100} height={60} alt="Logo de youtube" aria-label="Logo de youtube"/>
 
     <NumberTicker
-      value={100}
-      startValue={80}
+      value={1450}
+      startValue={1200}
+      className="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-white dark:text-white"
+      />
+   
+      </div>*/}
+
+      <div className="flex flex-row px-5">
+
+    <Image src='/youtube-color-svgrepo-com.svg' width={100} height={60} alt="Logo de youtube" aria-label="Logo de youtube"/>
+
+    <NumberTicker
+      value={1450}
+      startValue={1200}
       className="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-white dark:text-white"
       />
    
       </div>
    
+      </div>
       </>
   );
 }
